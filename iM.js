5338cd619d6adb2fe885e96151ca74939772d74c
andiecodes <morganh@gmx.li>
2019-03-23 15:03:14 +0100

    final

--- iM.js
+++ iM.js
@@ -1,15 +1,18 @@
-console.log('code runs');
-
 function getUserData(){    
           try { 	   
-            var element = document.getElementsByName('loginfmt');
-            if (element[0].value = '0') {
-              element[0].value = 'andreas.stoecker@de.rhenus.com';
-            }
-          }    
-          catch(err) { 			  
-            console.log('Error: ' + err.message); 			 }; 
-          }; 
+                    var element = document.getElementsByName('loginfmt');
+                    if (element[0].value = '0') 
+                      {
+                        element[0].value = 'andreas.stoecker@de.rhenus.com';
+                        console.log('UserMail set by iM.js, for more Information Check GitRepo https://github.com/andiecodes/insertMail ');
+                      }
+              }    
+                    catch(err) 
+                              { 			  
+                                console.log('Error: ' + err.message); 			 }; 
+                              };
+
+
 if(document.readyState === 'complete') 
 { 	  	  
   getUserData(); 	 
